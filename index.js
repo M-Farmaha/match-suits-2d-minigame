56b0c25379c725fb3b817c27978b57e8b658b566
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-11-14 21:17:44 +0200

    complete main task + added animation and score

--- index.js
+++ index.js
@@ -1,29 +1,155 @@
 "use strict";
 
-const container = document.querySelector(".container");
-const suits = ["hearts", "diamonds", "clubs", "spades"];
-const rows = 7;
-const columns = 6;
+class CardGame {
+  constructor(container, rows, columns) {
+    this.container = container;
+    this.rows = rows;
+    this.columns = columns;
+    this.score = 0;
+    this.suits = ["hearts", "diamonds", "clubs", "spades"];
 
-const handleButtonClick = (event) => {
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
 
-    if (!event.target.closest("button")) {
-      return;
+    this.container.addEventListener("click", this.handleButtonClick);
+    this.container.addEventListener("mouseover", this.handleMouseOver);
+    this.container.addEventListener("mouseout", this.handleMouseOut);
+
+    this.createCards();
+  }
+
+  handleMouseOver(event) {
+    if (event.target.nodeName !== "BUTTON") return;
+
+    const { suit } = event.target.dataset;
+    const allCards = this.container.querySelectorAll(".card");
+    const currentIndex = Array.from(allCards).indexOf(event.target);
+    const adjasentIndexesArray = [currentIndex];
+
+    let i = 0;
+
+    while (i < adjasentIndexesArray.length) {
+      const index = adjasentIndexesArray[i];
+
+      if (
+        allCards[index + 1]?.dataset.suit === suit &&
+        (index + 1) % this.columns !== 0 &&
+        !adjasentIndexesArray.includes(index + 1)
+      ) {
+        adjasentIndexesArray.push(index + 1);
+      }
+
+      if (
+        allCards[index - 1]?.dataset.suit === suit &&
+        index % this.columns !== 0 &&
+        !adjasentIndexesArray.includes(index - 1)
+      ) {
+        adjasentIndexesArray.push(index - 1);
+      }
+
+      if (
+        allCards[index + this.columns]?.dataset.suit === suit &&
+        !adjasentIndexesArray.includes(index + this.columns)
+      ) {
+        adjasentIndexesArray.push(index + this.columns);
+      }
+
+      if (
+        allCards[index - this.columns]?.dataset.suit === suit &&
+        !adjasentIndexesArray.includes(index - this.columns)
+      ) {
+        adjasentIndexesArray.push(index - this.columns);
+      }
+
+      i += 1;
     }
-    console.log("click");
-  };
 
-container.addEventListener("click", handleButtonClick);
+    allCards.forEach((card, index) => {
+      if (adjasentIndexesArray.includes(index)) {
+        card.classList.add("hovered");
+      }
+    });
+  }
 
-for (let i = 0; i < rows * columns; i += 1) {
-  const cardEl = document.createElement("button");
-  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
-  const svgPath = `svg/${randomSuit}.svg`;
-  const svgElement = document.createElement("img");
+  handleMouseOut() {
+    const allCards = this.container.querySelectorAll(".card");
 
-  cardEl.classList.add("card", randomSuit);
-  svgElement.src = svgPath;
+    allCards.forEach((card) => {
+      card.classList.remove("hovered");
+    });
+  }
 
-  cardEl.appendChild(svgElement);
-  container.appendChild(cardEl);
+  handleButtonClick(event) {
+    if (event.target.nodeName !== "BUTTON") return;
+
+    const allCards = this.container.querySelectorAll(".card");
+
+    allCards.forEach((card) => {
+      if (card.classList.contains("hovered")) {
+        this.score += 1;
+
+        card.classList.remove("hovered");
+        this.changeCardElement(card);
+      }
+    });
+
+    setTimeout(() => {
+      this.handleMouseOver(event);
+    }, 250);
+
+    this.updateScore()
+  }
+
+  changeCardElement(cardElement) {
+    const currentSuit = cardElement.dataset.suit;
+    const newSuits = this.suits.filter((suit) => suit !== currentSuit);
+    const suit = newSuits[Math.floor(Math.random() * newSuits.length)];
+
+    const svgPath = `svg/${suit}.svg`;
+    const imgElement = cardElement.querySelector("img");
+
+    cardElement.classList.add("hidden");
+
+    setTimeout(() => {
+      cardElement.dataset.suit = suit;
+      imgElement.src = svgPath;
+      imgElement.alt = suit;
+      cardElement.classList.remove("hidden");
+    }, 200);
+  }
+
+  createCardElement() {
+    const suit = this.suits[Math.floor(Math.random() * this.suits.length)];
+    const cardElement = document.createElement("button");
+    const svgPath = `svg/${suit}.svg`;
+    const imgElement = document.createElement("img");
+
+    imgElement.setAttribute("src", svgPath);
+    imgElement.setAttribute("alt", suit);
+    imgElement.style.pointerEvents = "none";
+
+    cardElement.classList.add("card");
+    cardElement.dataset.suit = suit;
+    cardElement.style.flex = `1 0 calc(100% / ${this.columns} - 5px)`;
+
+    cardElement.appendChild(imgElement);
+
+    return cardElement;
+  }
+
+  createCards() {
+    for (let i = 0; i < this.rows * this.columns; i += 1) {
+      const cardElement = this.createCardElement();
+      this.container.appendChild(cardElement);
+    }
+  }
+
+  updateScore() {
+    const scoreElement = document.querySelector(".score");
+    scoreElement.textContent = `Score: ${this.score}`;
+  }
 }
+
+const container = document.querySelector(".container");
+const cardGame = new CardGame(container, 7, 6);
